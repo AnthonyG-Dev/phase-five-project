3c069677e6f1020efc85b7e4fde6e79c9e419b22
MaryKaheti <kahetikihara@gmail.com>
2023-08-01 15:02:41 +0300

    Interactive Navbar

--- src/App.js
+++ src/App.js
@@ -5,10 +5,17 @@ import SignUp from './commponents/SignUp';
 import LogIn from './commponents/LogIn';  
 import Header from './commponents/Header';
 import Main from './commponents/Main';
+import Announcement from "./routes/Announcement";
+import Home from "./routes/Home";
+import Calendar from "./routes/Calendar";
+import Feed from "./routes/Feed";
+import Navbar from './commponents/Navbar';
 
 function App() {
-  return (    
-   <Router>    
+  return ( 
+    <>
+    <Navbar />
+       
         <Routes>
         <Route path="/" element={
             <Main/>
@@ -19,9 +26,22 @@ function App() {
           <Route path="/login" element={
           <LogIn/>
           } />
+          <Route path="/" element={
+          <Home/>
+          } />
+          <Route path="/calendar" element={
+          <Calendar/>
+          } />
+          <Route path="/announcement" element={
+          <Announcement/>
+          } />
+          <Route path="/feed" element={
+          <Feed/>
+          } />
         </Routes>
         <Footer/>
-      </Router>   
+        
+      </> 
   );
 }
 
