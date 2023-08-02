cde82494ecd9e4b0f182e10d66560023acd49da6
Your Name <you@example.com>
2023-08-02 02:33:11 -0400

    Update code

--- src/commponents/Main.js
+++ src/commponents/Main.js
@@ -1,17 +1,15 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'  
+import '../css/Main.css'
 
 function Main() {
   return (
     <header className="App-header">
     <img src="#" alt="" />
-    <p>
-      <code>Welcome to Class Sync</code>
-    </p>
-    <h2>Class Sync</h2>
-    <p>
-      <code>Sync your schedule, ace your classes</code>
-    </p>
+      <p>Welcome to Class Sync</p>
+      <h2>Class Sync</h2>
+      <p>Sync your schedule, ace your classes</p>
+  
     <div className="btn-group">
       <NavLink to="/signup"> {/* Link the button to the "/signup" route */}
         <button>SignUp</button>
