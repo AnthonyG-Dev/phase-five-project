53f843d671439305f6ac1fdb92789e1e64f875a5
MaryKaheti <kahetikihara@gmail.com>
2023-08-02 13:39:36 +0300

    Update Calendar

--- src/routes/Calendar.js
+++ src/routes/Calendar.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import Fullcalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
 
 function Calendar() {
   return (
     <div>
-      <h1>Calendar</h1>
+      <Fullcalendar
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+        initialView={"dayGridMonth"}
+        headerToolbar={{
+          start: "today prev,next", // will normally be on the left. if RTL, will be on the right
+          center: "title",
+          end: "dayGridMonth,timeGridWeek,timeGridDay", // will normally be on the right. if RTL, will be on the left
+        }}
+        height={"80vh"}
+      />
     </div>
   )
 }
